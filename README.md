c81c233122e8e63e060f84fce3c3e52f1caeea13
pgrabarczyk <grabarczyk.patryk@gmail.com>
2022-02-06 23:48:52 +0100

    Added DEBUG param

--- README.md
+++ README.md
@@ -6,12 +6,28 @@ Example usage:
 ```bash
 # Split "sample.csv" to files. Each output file should have 3 lines (+header) and be named with prefix "result_".
 pgrabarczyk@pgrabarczyk:~/git/csv-file-splitter$ bash csv-file-splitter.sh "sample.csv" 3 "result_"
+source_csv_path: sample.csv
 dest_prefix_path: /home/pgrabarczyk/git/csv-file-splitter/output/result_
 There is 7 lines in file. I'll create 2 files per 3 (Last file may have less)
+Start processing.
+Finished.
+```
+
+The same with DEBUG mode (last parameter set to 1)
+```bash
+pgrabarczyk@pgrabarczyk:~/git/csv-file-splitter$ bash csv-file-splitter.sh "sample.csv" 3 "result_" 1
+source_csv_path: sample.csv
+dest_prefix_path: /home/pgrabarczyk/git/csv-file-splitter/output/result_
+There is 7 lines in file. I'll create 2 files per 3 (Last file may have less)
+Start processing.
 Creating file /home/pgrabarczyk/git/csv-file-splitter/output/result_000001.csv with lines [1;4]
 Creating file /home/pgrabarczyk/git/csv-file-splitter/output/result_000002.csv with lines [4;7]
 Creating file /home/pgrabarczyk/git/csv-file-splitter/output/result_000003.csv with lines [7;10]
+Done.
+```
 
+Results:
+```bash
 pgrabarczyk@pgrabarczyk:~/git/csv-file-splitter$ cat output/result_00000{1..3}.csv 
 CSVLine,Anything
 2,A
--- csv-file-splitter.sh
+++ csv-file-splitter.sh
@@ -4,11 +4,13 @@ set -eu
 SOURCE_CSV_PATH="${1}"
 LINES_PER_FILE="${2}"
 DEST_PREFIX_NAME="${3}"
+DEBUG="${4:-0}"
 
 split_files() {
   local source_csv_path="${1}"
   local lines_per_file="${2}"
   local dest_prefix_name="${3}"
+  local debug="${4}"
 
   echo "source_csv_path: ${source_csv_path}"
   local dest_prefix_path="$(pwd)/output/${dest_prefix_name}"
@@ -23,17 +25,24 @@ split_files() {
   local files_to_create=$(((lines_in_files-1)/lines_per_file))
   echo "There is ${lines_in_files} lines in file. I'll create ${files_to_create} files per ${lines_per_file} (Last file may have less)"
 
+  echo "Start processing."
+
   for (( start_line=1; start_line<=lines_in_files; )); do
     last_line=$((start_line+lines_per_file))
     file_no=$((file_no+1))
     local file_path="${dest_prefix_path}$(printf "%06d" ${file_no}).csv"
-    echo "Creating file ${file_path} with lines [${start_line};${last_line}]"
+
+    if [ $debug -eq 1 ]; then
+      echo "Creating file ${file_path} with lines [${start_line};${last_line}]"
+    fi
 
     echo "${headline}" > "${file_path}"
     awk "NR>${start_line} && NR<=${last_line}" "${source_csv_path}" >> "${file_path}"
 
     start_line=$last_line
   done
+
+  echo "Done."
 }
 
-split_files "${SOURCE_CSV_PATH}" "${LINES_PER_FILE}" "${DEST_PREFIX_NAME}"
+split_files "${SOURCE_CSV_PATH}" "${LINES_PER_FILE}" "${DEST_PREFIX_NAME}" "${DEBUG}"
