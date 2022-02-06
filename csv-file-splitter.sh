b89ec88e39e25e4546ff8814a70eb3f8580ade18
pgrabarczyk <grabarczyk.patryk@gmail.com>
2022-02-06 23:55:28 +0100

    Added date & time for logs

--- csv-file-splitter.sh
+++ csv-file-splitter.sh
@@ -12,9 +12,9 @@ split_files() {
   local dest_prefix_name="${3}"
   local debug="${4}"
 
-  echo "source_csv_path: ${source_csv_path}"
+  _print_log "source_csv_path: ${source_csv_path}"
   local dest_prefix_path="$(pwd)/output/${dest_prefix_name}"
-  echo "dest_prefix_path: ${dest_prefix_path}"
+  _print_log "dest_prefix_path: ${dest_prefix_path}"
 
   local headline=$(awk "NR==1" "${source_csv_path}")
   local file_no=0
@@ -23,9 +23,9 @@ split_files() {
 
   local lines_in_files=$(wc -l "${source_csv_path}" | awk '{print $1}')
   local files_to_create=$(((lines_in_files-1)/lines_per_file))
-  echo "There is ${lines_in_files} lines in file. I'll create ${files_to_create} files per ${lines_per_file} (Last file may have less)"
+  _print_log "There is ${lines_in_files} lines in file. I'll create ${files_to_create} files per ${lines_per_file} (Last file may have less)"
 
-  echo "Start processing."
+  _print_log "Start processing."
 
   for (( start_line=1; start_line<=lines_in_files; )); do
     last_line=$((start_line+lines_per_file))
@@ -33,7 +33,7 @@ split_files() {
     local file_path="${dest_prefix_path}$(printf "%06d" ${file_no}).csv"
 
     if [ $debug -eq 1 ]; then
-      echo "Creating file ${file_path} with lines [${start_line};${last_line}]"
+      _print_log "Creating file ${file_path} with lines [${start_line};${last_line}]"
     fi
 
     echo "${headline}" > "${file_path}"
@@ -42,7 +42,13 @@ split_files() {
     start_line=$last_line
   done
 
-  echo "Done."
+  _print_log "Done."
+}
+
+_print_log() {
+  local log_message="${1}"
+  local date_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+  printf "%s - %s\n" "${date_time}" "${log_message}" >&2
 }
 
 split_files "${SOURCE_CSV_PATH}" "${LINES_PER_FILE}" "${DEST_PREFIX_NAME}" "${DEBUG}"
